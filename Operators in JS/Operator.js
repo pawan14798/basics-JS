9ef7383dbdaa27b68ef64bfb6d814634d576b580
Pawan <pawan18ai@gmail.com>
2026-05-19 19:31:57 +0530

    Add variable concepts, literals, and operators JS examples

---
+++ Operators in JS/Operator.js
@@ -0,0 +1,66 @@
+// Assignment Operators
+// -  = 
+// - to assign the right hand side value to the left side.
+// - let x = 10;
+// - x = 30;
+
+let x = 10;
+x = 11;
+x = 90;
+console.log(x);
+
+// Arithmetic Operators
+
+let a = 10, b = 3;
+console.log(a);
+console.log(b);
+
+let sum = a + b;
+let sub = a - b;
+let mul = a * b;
+let div = a / b;
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);  q
+
+// % = Modulus
+// a%b -> it will return the remainder
+
+
+let result = 13 % 7;
+console.log(result);
+
+
+console.log(101 % 2);
+console.log(100 % 2);
+console.log(37 % 2);
+console.log(36 % 2);
+
+//  n%2 == 1 - odd number, [ n%2 == 0 , even]
+
+console.log(2 ** 3);
+// 2^3
+
+let x = 10;
+let y = 3;
+console.log(x ** y);
+
+// Compund Operator
+
+let x = 10;
+x += 10; // x = x +10;
+console.log(x);
+
+x -= 3; //  x =x -3
+console.log(x);
+
+x *= 2; // x = x * 2;
+console.log(x);
+
+x /= 17;  // x = x/17;
+console.log(x);
+
+x %= 2;
+console.log(x); q
---
+++ Operators in JS/Strin_Concat.js
@@ -0,0 +1,6 @@
+let s = "Hi";
+s += " Dev";
+console.log(s);
+
+console.log(null >= 0); //  null == 0 or null > 0
+console.log(null === 0);
