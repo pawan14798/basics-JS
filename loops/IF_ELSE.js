ade328d394e61a261fe70114a22eaf218c4283ab
Pawan <pawan18ai@gmail.com>
2026-05-20 08:27:30 +0530

    Restructure JS basics into organized folders (Functions, Hosting, Variables, loops)

---
+++ loops/IF_ELSE.js
@@ -0,0 +1,37 @@
+
+//1st task
+let number=7;
+if(number%2===0)
+{
+    console.log("even number");
+}else
+{
+    console.log("this is not even number");
+}
+
+
+//2 task
+let number=85;
+    if(number>=90)
+    {
+        console.log("A grade");
+    }else if(number>=80 && number<90)
+    {
+        console.log("B Grade")
+    }else if(number>=70 && number<80)
+    {
+        console.log("c grade");
+    }else
+    {
+        console.log("D Grade");
+    }
+
+    //3rd task  leap year checker
+    let year=1998;
+    if((year%4===0 && year%100!=0)||year%400===0)
+{
+    console.log("leap year");
+}else
+{
+    console.log("not a leap year");
+}
